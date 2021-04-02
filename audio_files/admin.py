4faf47ba44bf4cd21a0332d591f293a0e5973752
Amal V Nair <amalvnair95@gmail.com>
2021-04-02 14:14:02 +0530

    Added Audiobook model and admin

--- audio_files/admin.py
+++ audio_files/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Song, Podcast
+from .models import Song, Podcast, AudioBook
 
 # Register your models here.
 
@@ -11,5 +11,10 @@ class PodcastAdmin(admin.ModelAdmin):
     readonly_fields = ('id', )
     list_display = ('__str__', 'id', 'name', 'duration', 'uploadedtime', 'host', 'participants', )
 
+class AudioBookAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', )
+    list_display = ('__str__', 'id', 'name', 'duration', 'uploadedtime', 'author', 'narrator', )
+
 admin.site.register(Song, SongAdmin)
 admin.site.register(Podcast, PodcastAdmin)
+admin.site.register(AudioBook, AudioBookAdmin)
--- audio_files/models.py
+++ audio_files/models.py
@@ -41,3 +41,11 @@ class Podcast(Attributes):
 
     def __str__(self):
         return f"PODCAST - {self.name}"
+
+
+class AudioBook(Attributes):
+    author = models.CharField(max_length=100, blank=False, null=False)
+    narrator = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return f"AUDIOBOOK - {self.name}"
